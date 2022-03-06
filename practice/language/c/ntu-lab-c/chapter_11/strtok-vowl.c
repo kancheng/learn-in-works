47f2d1c60fbf4cceabbb5ce50ac86e215c172cbb
kancheng <zxdfgcv@gmail.com>
2022-03-06 21:15:27 +0800

    Added the ntu practice

---
+++ practice/language/c/ntu-lab-c/chapter_11/strtok-vowl.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#define PATHLEN 40
+#define FILE 40
+int main (void) {
+	char pathname[PATHLEN];
+	scanf("%s", pathname);
+	char file[FILE][PATHLEN];
+	int fileCount = 0;
+	char *start = pathname;
+	char delimeters[] = "aeiou";
+	start = strtok(start, delimeters);
+	while (start != NULL) {
+		strcpy(file[fileCount], start);
+		fileCount++;
+		assert (fileCount < FILE);
+		start = strtok(NULL,delimeters);
+	}
+	for (int i = 0; i < fileCount; i++)
+		printf("%s\n", file[i]);
+	printf("After strtok pathname become %s\n", pathname);
+	return 0;
+}
